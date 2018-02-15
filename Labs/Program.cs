fc2762604a8ae006493c4738896ef72dfe360072
James Gordon <james.gordon1628@gmail.com>
2018-02-15 20:10:50 +0000

    Added empty project

---
+++ Labs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Labs
+{
+    class Program
+    {
+        
+
+        #region No Changes Required Here Ever!
+        /* 
+         * By all means look at this code, but you will not be required to change
+         * any of this code
+         */
+       
+        static void Main(string[] args)
+        {
+            GameWindow window = new ACW.BuildWindow();
+                   
+
+            if (window != null)
+            {
+                window.Run();
+            }
+        }
+        #endregion
+    }
+}
